fb4f316e1271c29435531fc7451747c55416c974
Priyank R Diyora <az.priyankdiyora@gmail.com>
2026-05-13 19:19:54 +0530

    chore(app): remove StaffToolbar component and usage from App

--- seva-connect-fronted/src/App.jsx
+++ seva-connect-fronted/src/App.jsx
@@ -1,11 +1,9 @@
 import AppRoutes from "./routes/AppRoutes";
-import { StaffToolbar } from "./components/StaffToolbar";
 
 function App() {
   return (
     <>
       <AppRoutes />
-      <StaffToolbar />
     </>
   );
 }
